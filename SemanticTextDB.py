cee382a5b8164c26949a4ed9099aec943896f265
Jonas Mueller <jonasmueller303@hotmail.com>
2014-11-24 00:36:58 -0500

    added more documentation notes

--- SemanticTextDB.py
+++ SemanticTextDB.py
@@ -4,6 +4,55 @@ import psycopg2
 
 class SemanticTextDB:
 	
+	"""
+	SemanticTextDB offers a Python interface with a Postgres database ("the underlying DB")
+	which relies on commands in the psycopg2 Python package to modify/query tables within 
+	the database.  SemanticTextDB methods are solely designed for
+	storage and analysis of text documents, so other types of data the user wishes
+	to store in the same underlying DB (i.e. non-document-related schemas) 
+	should be created and interacted-with in the usual psycopg fashion,
+	via the cursor class.  See http://initd.org/psycopg/docs/ for more information.
+	
+	SemanticTextDB allows the user to create a special Postgres table to store 
+	documents and relevant meta-data, the DocumentTable.  As it is simply a Postgres
+	table, a DocumentTable may be queried in the usual pyscopg fashion, 
+	but both insertion/deletion of documents + meta-data and creation/removal of a
+	DocumentTable should ONLY be done via SemanticTextDB functions rather than manually.
+	
+	When a DocumentTable called "d" is created, a Postgres table of the same name is
+	initialized in the underlying DB.  The tuples in this table contain a unique ID 
+	assigned to each document as well as other user-specified meta-data columns such as
+	author, title, publication date, etc.  Depending on user-selected options, 
+	SemanticTextDB is capable of automatically generating useful analytic representations 
+	corresponding to each DocumentTable, which are stored as machine-generated fields in 
+	tables or entirely new machine-generated Postgres schemas.  While these tables may be 
+	queried and their values-changed using psycopg (to improve model-parameters),
+	insertion/deletion from these machine-managed schemas is not allowed, and instead, 
+	users should use the options provided in SemanticTextDB to turn on/off 
+	different modeling capabilities.
+	
+	In the underlying DB, the names of the automatically machine-managed schemas 
+	corresponding to DocumentTable d will be:
+	
+	d_text : Tuples in this table contain a foreign-key document ID "id" pointing to table d 
+	and a "content" field which contains the actual text of the document.  
+	SemanticTextDB provides functions to nicely format this text for reading.
+	
+	d_word_counts : A table containing counts of each word over all documents in the 
+	DocumentTable and other word-specific information. TODO
+	
+	d_doc_entities : A table in which each tuple contains fields: "entity_name" (transformed
+	to adhere to a common spelling), the ID of the document referencing, the number of 
+	occurences in the document, the relative importance of the entity to the document. 
+	Since the same entity may be mentioned in multiple documents, multiple tuples may
+	share the same entity_name. TODO
+	
+	
+	d_topics_K : A table containing K fields, corresponding to a fitted topic model
+	  
+	The names of these schemas may also be accessed as fields the DocumentTable python object.
+	"""
+	
 	def __init__(self, conn, cursor, name = None):
 		self.conn = conn
 		self.cursor = cursor
@@ -69,7 +118,7 @@ class SemanticTextDB:
 						   summaries, topics, entities,
 						   sentiment,count_words, topicmodel, 
 						   entitymodel)
-		self.document_tables['name'] = dt
+		self.document_tables[name] = dt
 		return dt
 
 
@@ -87,7 +136,17 @@ class SemanticTextDB:
 		if table_name not in self.document_tables:
 			raise LookupError(table_name + " is an unknown table")
 		"""
-		Inserts a document into the DB and upates models
+		Inserts a document into the DB and upates models if specified
+		:param id: an integer which cannot be an existing document id in the DB
+		:param text: the text of the document (string)
+		"""
+		return 0
+		
+	def deleteDoc(self, id, table_name):
+		if table_name not in self.document_tables:
+			raise LookupError(table_name + " is an unknown table")
+		"""
+		Delects a document from the DB and upates models
 		:param id: an integer which cannot be an existing document id in the DB
 		:param text: the text of the document (string)
 		"""
